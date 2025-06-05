62b336175e00eea5b07fb6b36104e53979f66578
sinclair-29 <732277968@qq.com>
2025-06-05 16:43:48 +0800

    fix

--- llm_attacks/minimal_gcg/string_utils.py
+++ llm_attacks/minimal_gcg/string_utils.py
@@ -9,7 +9,7 @@ def load_conversation_template(template_name: str):
     return conv_template
 
 
-class PromptManager:
+class SuffixManager:
     def __init__(self, *, tokenizer, conv_template, instruction: str, target: str, adv_string: str):
         self.tokenizer = tokenizer
         self.conv_template = conv_template  # Store the original template
@@ -49,7 +49,7 @@ class PromptManager:
         self.conv_template.append_message(self.conv_template.roles[1], str_target_segment)
         self._full_prompt_str = self.conv_template.get_prompt()
 
-        tok_user_role_start = self.tokenizer(str_user_role_start_segment, add_special_tokens=False).input_ids
+        tok_user_role_start = self.tokenizer(str_user_role_start_segment, add_special_tokens=True).input_ids
         tok_instruction = self.tokenizer(str_instruction_segment, add_special_tokens=False).input_ids
         tok_adv_with_sep = self.tokenizer(str_adv_segment_with_separator, add_special_tokens=False).input_ids
         tok_assistant_role = self.tokenizer(str_assistant_role_segment, add_special_tokens=False).input_ids
@@ -76,11 +76,6 @@ class PromptManager:
         )
 
         return self._full_prompt_str
-
-    def get_input_ids(self, adv_string: str = None) -> torch.Tensor:
-        prompt_str = self.get_prompt(adv_string=adv_string)
-        input_ids_list = self.tokenizer(prompt_str, add_special_tokens=False).input_ids
-        return torch.tensor(input_ids_list[:self._target_slice.stop])
     
     def get_input_ids(self, adv_string=None, is_add_special_tokens = True):
         prompt = self.get_prompt(adv_string=adv_string)
@@ -113,14 +108,14 @@ def main():
     print("\n【各 slice 范围】:")
     print("User Role Slice:", manager._user_role_slice)
     print("Goal Slice:", manager._goal_slice)
-    print("Control Slice:", manager._control_slice)
+    print("Control Slice:", manager._adv_slice)
     print("Assistant Role Slice:", manager._assistant_role_slice)
     print("Target Slice:", manager._target_slice)
     print("Loss Slice:", manager._loss_slice)
 
     print("User Role Slice内容:", decode_slice(manager._user_role_slice))
     print("Goal Slice内容:", decode_slice(manager._goal_slice))
-    print("Control Slice内容:", decode_slice(manager._control_slice))
+    print("Control Slice内容:", decode_slice(manager._adv_slice))
     print("Assistant Role Slice内容:", decode_slice(manager._assistant_role_slice))
     print("Target Slice内容:", decode_slice(manager._target_slice))
     print("Loss Slice内容:", decode_slice(manager._loss_slice))
