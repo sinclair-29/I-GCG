9492fbe48e0ab322d90bbb67e2d160f81625d0db
sinclair-29 <732277968@qq.com>
2025-06-05 09:49:29 +0800

    verify the correctness

--- llm_attacks/minimal_gcg/string_utils.py
+++ llm_attacks/minimal_gcg/string_utils.py
@@ -58,90 +58,27 @@ class SuffixManager:
             self.conv_template.messages = []
             test = "a"
             self.conv_template.append_message(self.conv_template.roles[0], test)
-            toks = self.tokenizer(self.conv_template.get_prompt(), add_special_tokens = False).input_ids
+            toks = self.tokenizer(self.conv_template.get_prompt()).input_ids
             test_toks = self.tokenizer(test).input_ids
             self._user_role_slice = slice(None, len(toks) - len(test_toks))
 
             self.conv_template.update_last_message(f"{self.instruction}")
-            toks = self.tokenizer(self.conv_template.get_prompt(), add_special_tokens=False ).input_ids
+            toks = self.tokenizer(self.conv_template.get_prompt()).input_ids
             self._goal_slice = slice(self._user_role_slice.stop, max(self._user_role_slice.stop, len(toks)))
 
             separator = ' ' if self.instruction else ''
             self.conv_template.update_last_message(f"{self.instruction}{separator}{self.adv_string}")
-            toks = self.tokenizer(self.conv_template.get_prompt(), add_special_tokens=False ).input_ids
+            toks = self.tokenizer(self.conv_template.get_prompt()).input_ids
             self._control_slice = slice(self._goal_slice.stop, len(toks))
 
             self.conv_template.append_message(self.conv_template.roles[1], test)
-            toks = self.tokenizer(self.conv_template.get_prompt(), add_special_tokens=False).input_ids
+            toks = self.tokenizer(self.conv_template.get_prompt()).input_ids
             self._assistant_role_slice = slice(self._control_slice.stop, len(toks) - len(test_toks))
 
             self.conv_template.update_last_message(f"{self.target}")
-            toks = self.tokenizer(self.conv_template.get_prompt(), add_special_tokens=False ).input_ids
+            toks = self.tokenizer(self.conv_template.get_prompt()).input_ids
             self._target_slice = slice(self._assistant_role_slice.stop, len(toks)-2)
             self._loss_slice = slice(self._assistant_role_slice.stop-1, len(toks)-3)
-
-        else:
-            python_tokenizer = False or self.conv_template.name == 'oasst_pythia'
-            try:
-                encoding.char_to_token(len(prompt)-1)
-            except:
-                python_tokenizer = True
-
-            if python_tokenizer:
-                # This is specific to the vicuna and pythia tokenizer and conversation prompt.
-                # It will not work with other tokenizers or prompts.
-                self.conv_template.messages = []
-
-                self.conv_template.append_message(self.conv_template.roles[0], None)
-                toks = self.tokenizer(self.conv_template.get_prompt()).input_ids
-                self._user_role_slice = slice(None, len(toks))
-
-                self.conv_template.update_last_message(f"{self.instruction}")
-                toks = self.tokenizer(self.conv_template.get_prompt()).input_ids
-                self._goal_slice = slice(self._user_role_slice.stop, max(self._user_role_slice.stop, len(toks)-1))
-
-                separator = ' ' if self.instruction else ''
-                self.conv_template.update_last_message(f"{self.instruction}{separator}{self.adv_string}")
-                toks = self.tokenizer(self.conv_template.get_prompt()).input_ids
-                self._control_slice = slice(self._goal_slice.stop, len(toks)-1)
-
-                self.conv_template.append_message(self.conv_template.roles[1], None)
-                toks = self.tokenizer(self.conv_template.get_prompt()).input_ids
-                self._assistant_role_slice = slice(self._control_slice.stop, len(toks))
-
-                self.conv_template.update_last_message(f"{self.target}")
-                toks = self.tokenizer(self.conv_template.get_prompt()).input_ids
-                self._target_slice = slice(self._assistant_role_slice.stop, len(toks)-1)
-                self._loss_slice = slice(self._assistant_role_slice.stop-1, len(toks)-2)
-            else:
-                self._system_slice = slice(
-                    None, 
-                    encoding.char_to_token(len(self.conv_template.system))
-                )
-                self._user_role_slice = slice(
-                    encoding.char_to_token(prompt.find(self.conv_template.roles[0])),
-                    encoding.char_to_token(prompt.find(self.conv_template.roles[0]) + len(self.conv_template.roles[0]) + 1)
-                )
-                self._goal_slice = slice(
-                    encoding.char_to_token(prompt.find(self.instruction)),
-                    encoding.char_to_token(prompt.find(self.instruction) + len(self.instruction))
-                )
-                self._control_slice = slice(
-                    encoding.char_to_token(prompt.find(self.adv_string)),
-                    encoding.char_to_token(prompt.find(self.adv_string) + len(self.adv_string))
-                )
-                self._assistant_role_slice = slice(
-                    encoding.char_to_token(prompt.find(self.conv_template.roles[1])),
-                    encoding.char_to_token(prompt.find(self.conv_template.roles[1]) + len(self.conv_template.roles[1]) + 1)
-                )
-                self._target_slice = slice(
-                    encoding.char_to_token(prompt.find(self.target)),
-                    encoding.char_to_token(prompt.find(self.target) + len(self.target))
-                )
-                self._loss_slice = slice(
-                    encoding.char_to_token(prompt.find(self.target)) - 1,
-                    encoding.char_to_token(prompt.find(self.target) + len(self.target)) - 1
-                )
 
         self.conv_template.messages = []
         print("===========")
